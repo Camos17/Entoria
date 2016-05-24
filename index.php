830a51195b1b9b0016352835eea9cdaf5e4d2778
camos17 <camos8710@gmail.com>
2016-05-24 16:57:10 -0500

    Ajustes para la traduccion de algunos elementos de la pagina

--- index.php
+++ index.php
@@ -25,6 +25,7 @@
 		<meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
 		<meta name="theme-color" content="#ffffff ">	
 
+		<link href='https://fonts.googleapis.com/css?family=Anton' rel='stylesheet' type='text/css'>
 		<link rel="icon" type="image/png" href="img/favicon_entoria.png">	
 		<link rel="stylesheet" href="css/bootstrap.min.css">
 		<link rel="stylesheet" href="css/font-awesome.min.css">
@@ -75,8 +76,15 @@
 
 				<div class="col-xs-12 col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2 col-lg-6 col-lg-offset-3 contenido-info-entoria">
 					<div class="col-xs-12 img-proximamente">
-						<div class="col-xs-12 col-xs-10 col-xs-offset-1 col-lg-8 col-lg-offset-2">	
+						<div class="col-xs-12 col-xs-10 col-xs-offset-1 col-lg-8 col-lg-offset-2 proximamente-es">	
 							<img class="img-responsive" src="img/proximamente.svg" alt="...">
+						</div>
+						<div class="col-xs-12 col-xs-10 col-xs-offset-1 col-lg-8 col-lg-offset-2 proximamente-en">	
+							<img class="img-responsive" src="img/comingsoon.svg" alt="...">
+						</div>
+						<div class="col-xs-12 no-padding hidden">
+							<h2 class="headline1 traducir" key="proximamente">PRÓXIMAMENTE</h2>
+							<h3 class="headline2 traducir" key="pagina en construccion">Página en construcción</h3>
 						</div>
 					</div>
 					<div class="col-xs-12 info-entoria">	
@@ -122,7 +130,7 @@
 
 			</div>
 
-			<div class="col-xs-5 col-xs-offset-7 idiomas">
+			<div class="col-xs-5 col-xs-offset-7 col-sm-2 col-sm-offset-10 col-lg-1 col-lg-offset-11 idiomas">
 				<div class="col-xs-6 no-padding">
 					<a id="es" class="idioma btn btn-block no-padding" href="#" title="">ES</a>
 				</div>
@@ -155,9 +163,13 @@
 
 			aLangKeys['es']['gerente general']='Gerente General';
 			aLangKeys['es']['(asociado entoria)']='(Asociado Entoria)';
+			aLangKeys['es']['proximamente']='PRÓXIMAMENTE';
+			aLangKeys['es']['pagina en construccion']='Página en construcción';
 
 			aLangKeys['en']['gerente general']='General Manager';
 			aLangKeys['en']['(asociado entoria)']='(Partner of Entoria)';
+			aLangKeys['en']['proximamente']='COMING SOON';
+			aLangKeys['en']['pagina en construccion']='Website under construction';
 
 			$(document).ready(function(){
 
@@ -170,9 +182,26 @@
 		       		console.log(idioma);
 				});
 
+				$("#en").click(function(){
+					$(".proximamente-es").fadeOut(100, function(){
+						$(".proximamente-en").fadeIn(200);
+					});
+				});
 
-				// $(".idioma").click(function(){
-				// 	$(".span2").insertBefore($(".span1"));
+				$("#es").click(function(){
+					$(".proximamente-en").fadeOut(100, function(){
+						$(".proximamente-es").fadeIn(200);
+					});
+				});
+
+				
+				// $("#en").click(function(){
+				// 	$(".headline2").insertBefore($(".headline1"));
+				// 	$("h2").css({"margin-top":"0px"});
+				// });
+
+				// $("#es").click(function(){
+				// 	$(".headline1").insertBefore($(".headline2"));
 				// });
 
 			});		
